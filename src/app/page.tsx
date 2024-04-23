9e7af0364a3b1de0eafa8328edbce7db54366053
Nickrosendo <nicolasrosendo.dev@gmail.com>
2024-04-23 08:25:37 -0300

    fix: delete legacy component reference

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import styles from './page.module.css';
 import { HelloWorld } from '@root/src/components/HelloWorld';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <HelloWorld message="Foo" />
     </main>
   );
